1c1323b167d9432d2e40f2e287e19f93d51f96e5
Alex <alex-fkn@yandex.ru>
2012-09-25 10:23:24 +0400

    add django

---
+++ urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import *
+from mysite.views import hello, current_datetime, hour_ahead
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+  ('^hello/$',hello),
+  ('^time/$', current_datetime),
+  (r'^time/plus/(\d{1,2})/$', hour_ahead),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
